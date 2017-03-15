c2f0b66a0638f070e08cea862ee47805c2e54ada
cleyber <cleyber65@gmail.com>
2017-03-14 23:07:43 -0500

    Se agregan CDNs, se mejora la logica

--- js/index.js
+++ js/index.js
@@ -1,55 +1,39 @@
-
+Messenger.options = {
+    theme: 'air'
+};
 
 new Vue({
-    el: 'main', 
-    
+    el: 'main',
     data: {
         mensaje: '',
         nuevaTarea: '',
-        tareas: [{nombre: 'Unir Semantic con Vue', completado: false },{nombre: 'Realizar todo lo visto con Vue', completado: false}, {nombre: 'Hacer todoList', completado: false}]
+        id: 1,
+        tareas: []
     },
+   created() {
+      this.tareas.push({id: this.id++, nombre: 'Realizar todo lo visto con Vue', completado: false});
+      this.tareas.push({id: this.id++, nombre: 'Hacer todoList', completado: false});
+      this.tareas.push({id: this.id++, nombre: 'Mejorar la app', completado: false});
+   },
     methods:{
         agregarTarea(){
-            if(this.nuevaTarea.trim()){
-                this.tareas.push({nombre: this.nuevaTarea, completado: false});
+            if(this.nuevaTarea){
+                this.tareas.push({id: this.id++, nombre: this.nuevaTarea, completado: false});
                 this.nuevaTarea = '';
             }else{
-                Messenger.options = {
-                    extraClasses: 'messenger-fixed messenger-on-bottom messenger-on-right',
-                    theme: 'air'
-                }
-                Messenger().post({
+                Messenger().error({
                   message: 'Debe ingresar una tarea',
-                  type: 'error',
                   showCloseButton: true
                 });
-
-                this.mensaje = 'Debe ingresar una nueva tarea';
             }
         },
         completarTarea(tarea){
             tarea.completado = !tarea.completado;
         },
         eliminarTarea(tarea){
-            var titulo = tarea.nombre;
-            console.log(titulo);
+           this.tareas = this.tareas.filter(function(t){
+             return tarea.id !== t.id;
+          });
         }
     }
-});
-
-$(document).ready(function(){
-    $('#form-1')
-        .form({
-        fields: {
-            nuevaTarea: {
-                identifier: 'nuevaTarea',
-                rules: [
-                    {
-                        type: 'empty',
-                        prompt: 'Debes aagregar una nueva tarea'
-                    }
-                ]
-            }
-        }
-    });
 });
